ba4b0c31f81dddc9504d3205bec83853e6038517
Martia <pureromancebymartia@gmail.com>
2022-05-05 10:02:36 -0400

    welcome

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//dog shop codes
 var dogShop = [
   {
     title: 'Dogs',
@@ -16,13 +17,13 @@ var dogShop = [
     title: 'Dogs',
     image: './dog15petstore.jpg',
     price: '$2,500-$4,500',
-    description:'Thor'
+    description:'Skittles'
   },
   {
     title: 'Dogs',
     image: './dog18petstore.jpg',
     price: '$4,000-$6,000',
-    description:'Thor'
+    description:'Loki'
   },
   
   ]
@@ -44,3 +45,101 @@ var dogShop = [
       dogHTML = dogHTML + concatThis
   }
   document.getElementById('dogMarket').innerHTML = dogHTML
+
+  //cat shop codes
+
+  var catShop = [
+    {
+      title: 'Cats',
+      image: './cat10petstore.jpg',
+      price: '$2,000-$4,500',
+      description:'Panda'
+    },
+    
+    {
+      title: 'Cats',
+      image: './cat1petstore.jpg',
+      price: '$3,000-$5,500',
+      description:'Thor'
+    },
+    {
+      title: 'Cats',
+      image: './cat16petstore.jpg',
+      price: '$2,500-$4,500',
+      description:'Skittles'
+    },
+    {
+      title: 'Cats',
+      image: './cat12petstore.jpg',
+      price: '$4,000-$6,000',
+      description:'Loki'
+    },
+    
+    ]
+    
+    var catHTML = " "
+  
+  
+  
+  
+  
+  
+  
+    for (var i=0; i < catShop.length; i++){
+        var heading = '<div class="card ' + '"><h5>' + catShop[i].title + '</h5>'
+        var image = '<img class="card-img-top" src="' + catShop[i].image + '"/'
+        var price = '<p class="card-text"> $' + catShop[i].price + '</p>'
+        var description = '<div class="card-body"><p>'+ catShop[i].description;
+        var concatThis = heading + image + price + description;
+        catHTML = catHTML + concatThis
+    }
+    document.getElementById('catMarket').innerHTML = catHTML
+
+    //fish shop codes
+
+    var fishShop = [
+      {
+        title: 'Fish',
+        image: './fish11petstore.jpg',
+        price: '$2,000-$4,500',
+        description:'Panda'
+      },
+      
+      {
+        title: 'Fish',
+        image: './fish5petstore.jpg',
+        price: '$3,000-$5,500',
+        description:'Thor'
+      },
+      {
+        title: 'Fish',
+        image: './fish13petstore.jpg',
+        price: '$2,500-$4,500',
+        description:'Skittles'
+      },
+      {
+        title: 'Fish',
+        image: '/fish8petstore.jpg',
+        price: '$4,000-$6,000',
+        description:'Loki'
+      },
+      
+      ]
+      
+      var fishHTML = " "
+    
+    
+    
+    
+    
+    
+    
+      for (var i=0; i < fishShop.length; i++){
+          var heading = '<div class="card ' + '"><h5>' + fishShop[i].title + '</h5>'
+          var image = '<img class="card-img-top" src="' + fishShop[i].image + '"/'
+          var price = '<p class="card-text"> $' + fishShop[i].price + '</p>'
+          var description = '<div class="card-body"><p>'+ fishShop[i].description;
+          var concatThis = heading + image + price + description;
+          fishHTML = fishHTML + concatThis
+      }
+      document.getElementById('fishMarket').innerHTML = fishHTML
